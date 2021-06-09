acbf9c9a3a7ce55263856977c53f5fe7e324f73b
张亚飞 <672233580@qq.com>
2021-06-09 15:31:42 +0800

    Initial Commit

---
+++ SpotifyUI/SpotifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUIApp.swift
+//  SpotifyUI
+//
+//  Created by 张亚飞 on 2021/6/9.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
